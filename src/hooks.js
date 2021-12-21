19e0171bbff6676afe172bb97900244e72e4bb14
Alexander Aboutanos <alexanderaboutanos@gmail.com>
2021-12-21 17:08:20 -0500

    Step Two: useFlip

--- src/hooks.js
+++ src/hooks.js
@@ -1,3 +1,13 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
+
+function useFlip(initialState = true) {
+  const [state, setState] = useState(initialState);
+  const flip = () => {
+    setState((state) => !state);
+  };
+  return [state, flip];
+}
+
+export default useFlip;
